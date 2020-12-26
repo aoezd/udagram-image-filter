55659fc34a4df70a2b563d8fae9dcf6b7aa62731
Aykut Özdemir <oezdemir.aykut@web.de>
2020-12-26 14:08:57 +0100

    udagram-image-filter: Reviewer Feedback added
    Added missing types

--- src/server.ts
+++ src/server.ts
@@ -15,31 +15,34 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   /**
    * Validates a given image via an URL, filters the image and sends the resulting file as a response
    */
-  app.get('/filteredimage', async (req, res) => {
-    let { image_url } = req.query;
+  app.get(
+    '/filteredimage',
+    async (req: express.Request, res: express.Response) => {
+      let { image_url } = req.query;
 
-    if (!image_url) {
-      return res
-        .status(400)
-        .send({ message: 'image_url is required or malformed' });
-    }
+      if (!image_url) {
+        return res
+          .status(400)
+          .send({ message: 'image_url is required or malformed' });
+      }
 
-    await filterImageFromURL(image_url)
-      .then((filterImage) => {
-        res
-          .status(200)
-          .sendFile(filterImage, {}, () => deleteLocalFiles([filterImage]));
-      })
-      .catch((error) =>
-        res.status(422).send({
-          message: `Given image could not be processed: ${error}`,
+      await filterImageFromURL(image_url)
+        .then((filterImage) => {
+          res
+            .status(200)
+            .sendFile(filterImage, {}, () => deleteLocalFiles([filterImage]));
         })
-      );
-  });
+        .catch((error) =>
+          res.status(422).send({
+            message: `Given image could not be processed: ${error}`,
+          })
+        );
+    }
+  );
 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get('/', async (req, res) => {
+  app.get('/', async (req: express.Request, res: express.Response) => {
     res.send('try GET /filteredimage?image_url={{}}');
   });
 
